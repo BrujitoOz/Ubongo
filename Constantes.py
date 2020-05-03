598accc9437577598e123bfe6e14fddfa03c36bb
Hernán Razo <ozemblem66@gmail.com>
2020-05-03 00:55:03 -0500

    Add files via upload

---
+++ Constantes.py
@@ -0,0 +1,11 @@
+# Cualquier constante como los colores van aqui
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+WHITE = (255,255,255)
+ORANGE = (255,165,0)
+YELLOW = (255,255,0)
+PURPLE = (128,0,128)
+
+YTABLERO = 100
